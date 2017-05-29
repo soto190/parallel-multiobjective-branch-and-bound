soto190/parallel-multiobjective-branch-and-bound

commit 9f7c192805d5e683a1a321d9aea206072e6e5c96

B&B class: - Removed member object bestObjectivesFound solution. - In branch function renamed some variables. - In branch function now the best values found are obtained from the Pareto grid. - Added a counter of shared work.
Handler container class:
- Now stores the best values found in each objective.

Binary search function:
- Initialized variables.

Problem FJSSP class:
- Activated again the part for the estimation of the workloads.

ParallelBaB/BranchAndBound.cpp
@@ -17,12 +17,13 @@ ReadySubproblems globalPool;  /** intervals are the pending branches/subproblems
 HandlerContainer paretoContainer;
 
 BranchAndBound::BranchAndBound(const BranchAndBound& toCopy):
-    rank(toCopy.getRank()),
-    problem(toCopy.getProblem()),
-    fjssp_data(toCopy.getFJSSPdata()),
-    incumbent_s(toCopy.getIncumbentSolution()),
-    ivm_tree(toCopy.getIVMTree()),
-    currentLevel(toCopy.getCurrentLevel()){
+rank(toCopy.getRank()),
+problem(toCopy.getProblem()),
+fjssp_data(toCopy.getFJSSPdata()),
+incumbent_s(toCopy.getIncumbentSolution()),
+ivm_tree(toCopy.getIVMTree()),
+currentLevel(toCopy.getCurrentLevel()),
+shared_work(toCopy.getSharedWork()){
         
         totalLevels.store(toCopy.getNumberOfLevels());
         totalNodes.store(toCopy.getNumberOfNodes());
@@ -51,7 +52,8 @@ fjssp_data(problemToCopy.getNumberOfJobs(),
            problemToCopy.getNumberOfMachines()),
 interval_to_solve(branch),
 currentLevel(0),
-totalTime(0){
+totalTime(0),
+shared_work(0){
     
     totalLevels.store(problemToCopy.getNumberOfVariables());
     totalNodes.store(0);
@@ -76,13 +78,9 @@ totalTime(0){
     
     totalNodes.fetch_and_store(computeTotalNodes(numberOfVariables));
     
-    bestObjectivesFound(numberOfObjectives, numberOfVariables);
     incumbent_s(numberOfObjectives, numberOfVariables);
     problem.createDefaultSolution(incumbent_s);
-    
-    int nObj = 0;
-    for (nObj = 0; nObj < numberOfObjectives; ++nObj)
-        bestObjectivesFound.setObjective(nObj, incumbent_s.getObjective(nObj));
+    updateBoundsWithSolution(incumbent_s);
     
     ivm_tree(problem.getNumberOfVariables(), problem.getUpperBound(0) + 1);
     ivm_tree.setOwner(rank);
@@ -109,6 +107,7 @@ BranchAndBound& BranchAndBound::operator()(int rank_new, const ProblemFJSSP &pro
     callsToBranch = 0;
     totalUpdatesInLowerBound = 0;
     totalTime = 0;
+    shared_work = 0;
     
     interval_to_solve = branch; /** Copy the branch. **/
     
@@ -116,12 +115,7 @@ BranchAndBound& BranchAndBound::operator()(int rank_new, const ProblemFJSSP &pro
     int numberOfVariables = problem.getNumberOfVariables();
     
     incumbent_s(numberOfObjectives, numberOfVariables);
-    bestObjectivesFound(numberOfObjectives, numberOfVariables);
     problem.createDefaultSolution(incumbent_s);
-    
-    int nObj = 0;
-    for (nObj = 0; nObj < numberOfObjectives; ++nObj)
-        bestObjectivesFound.setObjective(nObj, incumbent_s.getObjective(nObj));
 
     ivm_tree(problem.getNumberOfVariables(), problem.getUpperBound(0) + 1);
     ivm_tree.setOwner(rank);
@@ -140,9 +134,6 @@ void BranchAndBound::initialize(int starts_tree) {
 
 	start = std::clock();
 
-	int numberOfObjectives = problem.getNumberOfObjectives();
-	int numberOfVariables = problem.getNumberOfVariables();
-
 	if (starts_tree == -1)
 		currentLevel = 0;
 	else
@@ -156,12 +147,7 @@ void BranchAndBound::initialize(int starts_tree) {
 	callsToBranch = 0;
 	totalUpdatesInLowerBound = 0;
 	totalNodes = computeTotalNodes(totalLevels);
-
-	bestObjectivesFound(numberOfObjectives, numberOfVariables);
-
-	int nObj = 0;
-	for (nObj = 0; nObj < numberOfObjectives; ++nObj)
-		bestObjectivesFound.setObjective(nObj, incumbent_s.getObjective(nObj));
+    shared_work = 0;
 
 }
 
@@ -201,6 +187,8 @@ void BranchAndBound::initGlobalPoolWithInterval(Interval & branch_to_split) {
     int element = 0;
     int machine = 0;
     int toAdd = 0;
+    
+    float distance_error[2];
     //fjssp_data.reset(); /** This function call is not necesary because the structuras starts empty.**/
     
     fjssp_data.setMinTotalWorkload(problem.getSumOfMinPij());
@@ -226,15 +214,18 @@ void BranchAndBound::initGlobalPoolWithInterval(Interval & branch_to_split) {
                 if (improvesTheGrid(incumbent_s)) {
                     /** Gets the branch to add. */
                     branch_to_split.setValueAt(split_level, toAdd);
-                    float distance_error_1 = (problem.getLowerBoundInObj(0) - fjssp_data.getMakespan()) / (float) problem.getLowerBoundInObj(0);
-                    float distance_error_2 = (problem.getLowerBoundInObj(1) - fjssp_data.getMaxWorkload()) / (float) problem.getLowerBoundInObj(1);
                     
-                    branch_to_split.setDistance(0, distance_error_1);
-                    branch_to_split.setDistance(1, distance_error_2);
+                    distance_error[0] = (problem.getLowerBoundInObj(0) - fjssp_data.getMakespan()) / (float) problem.getLowerBoundInObj(0);
+                    distance_error[1] = (problem.getLowerBoundInObj(1) - fjssp_data.getMaxWorkload()) / (float) problem.getLowerBoundInObj(1);
+                    
+                    branch_to_split.setDistance(0, distance_error[0]);
+                    branch_to_split.setDistance(1, distance_error[1]);
                     setPriorityTo(branch_to_split);
                     /** Add it to pending intervals. **/
-                    if (rank == 0)
+                    if (rank == 0){
                         globalPool.push(branch_to_split); /** The vector adds a copy of interval. **/
+                        shared_work++;
+                    }
                     branch_to_split.removeLastValue();
                     branches_created++;
                 } else
@@ -309,6 +300,7 @@ int BranchAndBound::intializeIVM_data(Interval& branch_init, IVMTree& tree){
             
             setPriorityTo(branch_init);
             globalPool.push(branch_init);
+            shared_work++;
             
             branch_init.removeLastValue();
             problem.evaluateRemoveDynamic(incumbent_s, fjssp_data, currentLevel + 1);
@@ -325,8 +317,7 @@ tbb::task* BranchAndBound::execute() {
         if(globalPool.try_pop(interval_to_solve))
             solve(interval_to_solve);
     
-    double elapsed_time =  getTotalTime();
-    printf("[B&B-%03d] No more intervals in global pool. Going to sleep. [ET: %6.6f sec.]\n", rank, elapsed_time);
+    printf("[B&B-%03d] No more intervals in global pool. Going to sleep. [ET: %6.6f sec.]\n", rank, getTotalTime());
     
     return NULL;
 }
@@ -409,11 +400,16 @@ int BranchAndBound::branch(Solution& solution, int currentLevel) {
     
     int branches_created = 0;
     
-    float distance_error_1 = 0, distance_error_2 = 0;
-    float distance_error_best_1 = 0, distance_error_best_2 = 0;
+    float distance_error[2];
+    float distance_error_best[2];
+    
     SortedVector sorted_elements;
     Data3 data;
 
+    int best_values_found[2];
+    for (int obj = 0; obj < 2; ++obj)
+        best_values_found[obj] = paretoContainer.getBestValueFoundIn(obj);
+    
     switch (problem.getType()) {
             
         case ProblemType::permutation:
@@ -447,22 +443,22 @@ int BranchAndBound::branch(Solution& solution, int currentLevel) {
                         solution.setVariable(currentLevel + 1, toAdd);
                         problem.evaluateDynamic(solution, fjssp_data, currentLevel + 1);
                         
-                        distance_error_best_1 = (problem.getBestMakespanFound() - solution.getObjective(0)) / (float) problem.getBestMakespanFound();
-                        distance_error_best_2 = (problem.getBestWorkloadFound() - solution.getObjective(1)) / (float) problem.getBestWorkloadFound();
+                        distance_error_best[0] = (best_values_found[0] - fjssp_data.getMakespan()) / (float) best_values_found[0];
+                        distance_error_best[1] = (best_values_found[1] - fjssp_data.getMaxWorkload()) / (float) best_values_found[1];
                         
-                        /** If the distance to LB is negative in both objectives then it cannot produce improvement. **/
-                        if ((distance_error_best_1 >= 0 || distance_error_best_2 >= 0) && improvesTheGrid(solution)) {
-                            
+                        /** If distance in obj1 is greater  or distance in ob2 is smaller the can produce an improvement. **/
+                        if ((distance_error_best[0] >= 0 || distance_error_best[1] <= 0) && improvesTheGrid(solution)) {
+                        
                             /** TODO: Here we can use a Fuzzy method to give priority to branches at the top or less priority to branches at bottom also considering the error or distance to the lower bound.**/
                             data.setValue(toAdd);
                             data.setObjective(0, fjssp_data.getMakespan());
                             data.setObjective(1, fjssp_data.getMaxWorkload());
                             
-                            distance_error_1 = (problem.getLowerBoundInObj(0) - data.getObjective(0)) / (float) problem.getLowerBoundInObj(0);
-                            distance_error_2 = (problem.getLowerBoundInObj(1) - data.getObjective(1)) / (float) problem.getLowerBoundInObj(1);
+                            distance_error[0] = (problem.getLowerBoundInObj(0) - data.getObjective(0)) / (float) problem.getLowerBoundInObj(0);
+                            distance_error[1] = (problem.getLowerBoundInObj(1) - data.getObjective(1)) / (float) problem.getLowerBoundInObj(1);
                         
-                            data.setDistance(0, distance_error_1);
-                            data.setDistance(1, distance_error_2);
+                            data.setDistance(0, distance_error[0]);
+                            data.setDistance(1, distance_error[1]);
 
                             sorted_elements.push(data, SORTING_TYPES::DIST_1);/** sorting the nodes to give priority to promising nodes. **/
                             
@@ -565,6 +561,7 @@ void BranchAndBound::shareWorkAndSendToGlobalPool(const Interval & branch_to_sol
                 
                 setPriorityTo(branch_to_send);
                 globalPool.push(branch_to_send);
+                shared_work++;
                 
                 //intervals_to_send.push_back(branch_to_send);
                 
@@ -580,7 +577,7 @@ void BranchAndBound::shareWorkAndSendToGlobalPool(const Interval & branch_to_sol
             }
             next_row++;
         }
-        /* In we need to sort the intervals.
+        /* If we need to sort the intervals.
         while(!intervals_to_send.empty()){
             globalPool.push(intervals_to_send.back());
             intervals_to_send.pop_back();
@@ -767,6 +764,7 @@ unsigned long BranchAndBound::getNumberOfUnexploredNodes( ) const{ return unexpl
 unsigned long BranchAndBound::getNumberOfPrunedNodes( ) const{ return prunedNodes; }
 unsigned long BranchAndBound::getNumberOfCallsToPrune( ) const{ return callsToPrune; }
 unsigned long BranchAndBound::getNumberOfUpdatesInLowerBound( ) const{ return totalUpdatesInLowerBound; }
+unsigned long BranchAndBound::getSharedWork() const{return shared_work;}
 
 void BranchAndBound::increaseNumberOfExploredNodes(unsigned long value){ exploredNodes.fetch_and_add(value); }
 void BranchAndBound::increaseNumberOfCallsToBranch(unsigned long value){ callsToBranch.fetch_and_add(value);}

ParallelBaB/BranchAndBound.hpp
@@ -63,6 +63,7 @@ private:
     tbb::atomic<unsigned long> callsToPrune;
     tbb::atomic<unsigned long> totalUpdatesInLowerBound;
     tbb::atomic<unsigned long> totalLevels; /** Number of tree levels. **/
+    unsigned long shared_work;
     
     int currentLevel; /** Active level **/
     
@@ -70,7 +71,6 @@ private:
     FJSSPdata fjssp_data;
     
     Solution incumbent_s;
-    Solution bestObjectivesFound;
     IVMTree ivm_tree;
     Interval interval_to_solve;
     std::vector<Solution> paretoFront; /** paretofFront. **/
@@ -115,6 +115,7 @@ public:
     unsigned long getNumberOfPrunedNodes() const;
     unsigned long getNumberOfCallsToPrune() const;
     unsigned long getNumberOfUpdatesInLowerBound() const;
+    unsigned long getSharedWork() const;
     
     void increaseNumberOfExploredNodes(unsigned long value);
     void increaseNumberOfCallsToBranch(unsigned long value);

ParallelBaB/ParallelBranchAndBound.cpp
@@ -16,6 +16,7 @@ ParallelBranchAndBound::~ParallelBranchAndBound(){}
 
 tbb::task * ParallelBranchAndBound::execute() {
 
+    unsigned long  shared_work = 0;
     globalPool.setSizeEmptying((unsigned long) (number_of_threads * 2)); /** If the global pool reach this size then the B&B starts sending part of their work to the global pool. **/
     
     BranchAndBound BB_container(0, problem, branch_init);
@@ -53,6 +54,7 @@ tbb::task * ParallelBranchAndBound::execute() {
         BB_container.increaseNumberOfPrunedNodes(bb_in->getNumberOfPrunedNodes());
         BB_container.increaseNumberOfReachedLeaves(bb_in->getNumberOfReachedLeaves());
         BB_container.increaseNumberOfUpdatesInLowerBound(bb_in->getNumberOfUpdatesInLowerBound());
+        shared_work += bb_in->getSharedWork();
 	}
     
     BB_container.setParetoFrontFile(outputParetoFile);
@@ -63,7 +65,7 @@ tbb::task * ParallelBranchAndBound::execute() {
 	BB_container.saveParetoFront();
 	BB_container.saveSummarize();
     bb_threads.clear();
-
+    printf("Shared work: %lu\n", shared_work);
     printf("Data swarm recollected and saved.\n");
 	printf("Parallel Branch And Bound ended.\n");
 	return NULL;

ParallelBaB.xcodeproj/xcuserdata/carlossoto.xcuserdatad/xcschemes/ParallelBaB.xcscheme
@@ -79,7 +79,7 @@
             isEnabled = "NO">
          </CommandLineArgument>
          <CommandLineArgument
-            argument = "/Users/carlossoto/Documents/workspace/Instances/FJSSP/Kacem/Kacem-I1.txt"
+            argument = "/Users/carlossoto/Documents/workspace/Instances/FJSSP/Kacem/Kacem-I0.txt"
             isEnabled = "YES">
          </CommandLineArgument>
          <CommandLineArgument

ParallelBaB/GridContainer.cpp
@@ -41,6 +41,9 @@ disabledBuckets(toCopy.disabledBuckets){
     for (indexx = 0; indexx < toCopy.getCols(); indexx++)
         rangeinx[indexx] = toCopy.rangeinx[indexx];
     
+    for (int obj = 0; obj < 2; ++obj)
+        best_value_found_in_obj[obj].fetch_and_store(toCopy.getBestValueFoundIn(obj));
+    
 }
 
 HandlerContainer::HandlerContainer(unsigned int rows, unsigned int cols, double maxValX, double maxValY):
@@ -73,6 +76,9 @@ grid(maxValX < cols?maxValX:cols, maxValY < rows?maxValY:rows) {
     
     for (divs = 1; divs < rows; divs++)
         rangeiny[divs] = rangeiny[divs - 1] + ry;
+    
+    for (int obj = 0; obj < 2; ++obj)
+        best_value_found_in_obj[obj].fetch_and_store(999999999);
 }
 
 HandlerContainer::~HandlerContainer() {
@@ -110,6 +116,9 @@ HandlerContainer& HandlerContainer::operator()(unsigned int rows, unsigned int c
     
     for (divs = 1; divs < rows; divs++)
         rangeiny[divs] = rangeiny[divs - 1] + ry;
+    
+    for (int obj = 0; obj < 2; ++obj)
+        best_value_found_in_obj[obj].fetch_and_store(999999999);
     
     return *this;
 }
@@ -167,8 +176,15 @@ int HandlerContainer::set(const Solution & solution, int x, int y) {
             break;
     }
     
+    if(updated)
+        for (int i = 0; i < 2; ++i)
+            if (solution.getObjective(i) < best_value_found_in_obj[i])
+                best_value_found_in_obj[i].fetch_and_store(solution.getObjective(i));
+    
     return updated;
 }
+
+int HandlerContainer::getBestValueFoundIn(int obj) const{return best_value_found_in_obj[obj];}
 
 /**
  * It uses a binary search tree locate the bucket which will contain the new solution.

ParallelBaB/GridContainer.hpp
@@ -306,7 +306,8 @@ private:
     tbb::atomic<unsigned long> activeBuckets;
     tbb::atomic<unsigned long> unexploredBuckets;
     tbb::atomic<unsigned long> disabledBuckets;
-	
+    tbb::atomic<int> best_value_found_in_obj [2]; /** for two objectives **/
+    
 	GridContainer grid;
 	std::vector<Solution> paretoFront;
 
@@ -342,7 +343,8 @@ public:
     void print() const;
 
 	double getMaxIn(int dimension);
-
+    int getBestValueFoundIn(int obj) const;
+    
 	std::vector<Solution>& getParetoFront();
 };
 

ParallelBaB/ProblemFJSSP.cpp
@@ -511,10 +511,9 @@ void ProblemFJSSP::evaluateDynamic(Solution &solution, FJSSPdata &data, int leve
         if(data.getWorkloadOnMachine(machine) > max_workload)
             max_workload = data.getWorkloadOnMachine(machine);
         
-/*        if(data.getTempBestWorkloadInMachine(machine) > max_workload)
-            max_workload = data.getTempBestWorkloadInMachine(machine);*/
+        if(data.getTempBestWorkloadInMachine(machine) > max_workload)
+            max_workload = data.getTempBestWorkloadInMachine(machine);
     }
-    
 
     data.setMakespan(makespan);
     data.setMaxWorkload(max_workload);
@@ -549,8 +548,8 @@ void ProblemFJSSP::evaluateRemoveDynamic(Solution & solution, FJSSPdata& data, i
         if(data.getWorkloadOnMachine(machine) > max_workload)
             max_workload = data.getWorkloadOnMachine(machine);
 
-/*        if(data.getTempBestWorkloadInMachine(machine) > max_workload)
-            max_workload = data.getTempBestWorkloadInMachine(machine);*/
+        if(data.getTempBestWorkloadInMachine(machine) > max_workload)
+            max_workload = data.getTempBestWorkloadInMachine(machine);
     }
   
     data.setMakespan(makespan);

ParallelBaB/myutils.cpp
@@ -36,15 +36,15 @@ double round_double(double value) {
 }
 
 int binarySearch(double value, const double * array, int size) {
-	int low, high, mid;
-
-	low = 0;
-	high = size - 1;
+	int low = 0, high = size - 1, mid;
 
 	if (value >= array[high])
 		return high;
     if(value <= array[low])
         return low;
+    
+    low++;
+    high--;
 
 	while (low <= high) {
 		mid = (low + high) * 0.5f;

commit 1a76c650a94e4318f04ca4956219cacc9b774c22

-Fixed a segmentation fault caused by commit 9f7c192. B&B class: - shared_work now is atomic. - shared_work is printed and saved in summarize file.
Paralle B&B:
- The constructor now receives the number of threads.

ParallelBaB/BranchAndBound.cpp
@@ -22,26 +22,25 @@ problem(toCopy.getProblem()),
 fjssp_data(toCopy.getFJSSPdata()),
 incumbent_s(toCopy.getIncumbentSolution()),
 ivm_tree(toCopy.getIVMTree()),
-currentLevel(toCopy.getCurrentLevel()),
-shared_work(toCopy.getSharedWork()){
-        
-        totalLevels.store(toCopy.getNumberOfLevels());
-        totalNodes.store(toCopy.getNumberOfNodes());
-        branches.store(toCopy.getNumberOfBranches());
-        exploredNodes.store(toCopy.getNumberOfExploredNodes());
-        reachedLeaves.store(toCopy.getNumberOfReachedLeaves());
-        unexploredNodes.store(toCopy.getNumberOfUnexploredNodes());
-        prunedNodes.store(toCopy.getNumberOfPrunedNodes());
-        callsToPrune.store(toCopy.getNumberOfCallsToPrune());
-        callsToBranch.store(toCopy.getNumberOfCallsToBranch());
-        totalUpdatesInLowerBound.store(toCopy.getNumberOfUpdatesInLowerBound());
-        start = toCopy.start;
-        
-        branches_to_move = problem.getUpperBound(0) * to_share;
-        deep_to_share = totalLevels * deep_limit_share;
+currentLevel(toCopy.getCurrentLevel()){
+    shared_work.store(toCopy.getSharedWork());
+    totalLevels.store(toCopy.getNumberOfLevels());
+    totalNodes.store(toCopy.getNumberOfNodes());
+    branches.store(toCopy.getNumberOfBranches());
+    exploredNodes.store(toCopy.getNumberOfExploredNodes());
+    reachedLeaves.store(toCopy.getNumberOfReachedLeaves());
+    unexploredNodes.store(toCopy.getNumberOfUnexploredNodes());
+    prunedNodes.store(toCopy.getNumberOfPrunedNodes());
+    callsToPrune.store(toCopy.getNumberOfCallsToPrune());
+    callsToBranch.store(toCopy.getNumberOfCallsToBranch());
+    totalUpdatesInLowerBound.store(toCopy.getNumberOfUpdatesInLowerBound());
+    start = toCopy.start;
     
-        std::strcpy(outputFile, toCopy.outputFile);
-        std::strcpy(summarizeFile, toCopy.summarizeFile);
+    branches_to_move = problem.getUpperBound(0) * to_share;
+    deep_to_share = totalLevels * deep_limit_share;
+    
+    std::strcpy(outputFile, toCopy.outputFile);
+    std::strcpy(summarizeFile, toCopy.summarizeFile);
 }
 
 BranchAndBound::BranchAndBound(int rank, const ProblemFJSSP& problemToCopy, const Interval & branch):
@@ -52,9 +51,8 @@ fjssp_data(problemToCopy.getNumberOfJobs(),
            problemToCopy.getNumberOfMachines()),
 interval_to_solve(branch),
 currentLevel(0),
-totalTime(0),
-shared_work(0){
-    
+totalTime(0){
+    shared_work.store(0);
     totalLevels.store(problemToCopy.getNumberOfVariables());
     totalNodes.store(0);
     branches.store(0);
@@ -775,6 +773,7 @@ void BranchAndBound::increaseNumberOfCallsToPrune(unsigned long value){ callsToP
 void BranchAndBound::increaseNumberOfPrunedNodes(unsigned long value){ prunedNodes.fetch_and_add(value); }
 void BranchAndBound::increaseNumberOfReachedLeaves(unsigned long value){ reachedLeaves.fetch_and_add(value); }
 void BranchAndBound::increaseNumberOfUpdatesInLowerBound(unsigned long value){ totalUpdatesInLowerBound.fetch_and_add(value); }
+void BranchAndBound::increaseSharedWork(unsigned long value){shared_work.fetch_and_add(value);}
 
 const Solution& BranchAndBound::getIncumbentSolution() const { return incumbent_s;}
 const IVMTree& BranchAndBound::getIVMTree() const { return ivm_tree;}
@@ -823,7 +822,8 @@ int BranchAndBound::saveSummarize() {
 			paretoContainer.getNumberOfDisabledBuckets());
 	printf("\tunexplored buckets:%ld\n",
 			paretoContainer.getNumberOfUnexploredBuckets());
-	printf("\tTotal elements in: %ld\n", paretoContainer.getSize());
+	printf("\tNumber of elements in: %ld\n", paretoContainer.getSize());
+    printf("Shared work: %ld\n", getSharedWork());
 	
     std::ofstream myfile(summarizeFile);
 	if (myfile.is_open()) {
@@ -841,6 +841,7 @@ int BranchAndBound::saveSummarize() {
 		myfile << "Leaves reached:      " << reachedLeaves << "\n";
 		myfile << "Updates in PF:       " << totalUpdatesInLowerBound
 				<< "\n";
+        myfile << "Shared work:         " << shared_work << "\n";
 		myfile << "Total time:          " << totalTime << "\n";
 
 		myfile << "Grid data:\n";
@@ -854,7 +855,6 @@ int BranchAndBound::saveSummarize() {
 				<< paretoContainer.getNumberOfUnexploredBuckets() << "\n";
 		myfile << "\tnumber of elements:\t" << paretoContainer.getSize()
 				<< "\n";
-
 		myfile << "The pareto front found is: \n";
 
 		int numberOfObjectives = problem.getNumberOfObjectives();
@@ -873,14 +873,14 @@ int BranchAndBound::saveSummarize() {
 					<< ++counterSolutions << " ";
 
 			for (nObj = 0; nObj < numberOfObjectives; ++nObj)
-				myfile << std::fixed << std::setw(26) << std::setprecision(16)
+				myfile << std::fixed << std::setw(6) << std::setprecision(0)
 						<< std::setfill(' ') << (*it).getObjective(nObj) << " ";
 
 			myfile << " | ";
 
 			for (nVar = 0; nVar < numberOfVariables; ++nVar)
 				myfile << std::fixed << std::setw(4) << std::setfill(' ')
-						<< (*it).getVariable(nVar) << " "; //printf("%3d ", (*it)->getVariable(nVar));
+						<< (*it).getVariable(nVar) << " ";
 
 			myfile << " |\n";
 		}

ParallelBaB/BranchAndBound.hpp
@@ -63,7 +63,7 @@ private:
     tbb::atomic<unsigned long> callsToPrune;
     tbb::atomic<unsigned long> totalUpdatesInLowerBound;
     tbb::atomic<unsigned long> totalLevels; /** Number of tree levels. **/
-    unsigned long shared_work;
+    tbb::atomic<unsigned long> shared_work;
     
     int currentLevel; /** Active level **/
     
@@ -124,6 +124,7 @@ public:
     void increaseNumberOfPrunedNodes(unsigned long value);
     void increaseNumberOfReachedLeaves(unsigned long value);
     void increaseNumberOfUpdatesInLowerBound(unsigned long value);
+    void increaseSharedWork(unsigned long value);
 
     const IVMTree& getIVMTree() const;
     const Interval& getStartingInterval() const;

ParallelBaB/ParallelBranchAndBound.cpp
@@ -8,15 +8,15 @@
 
 #include "ParallelBranchAndBound.hpp"
 
-ParallelBranchAndBound::ParallelBranchAndBound(const ProblemFJSSP& problem):
+ParallelBranchAndBound::ParallelBranchAndBound(int n_threads, const ProblemFJSSP& problem):
 problem(problem),
-branch_init(problem.getNumberOfVariables()){}
+branch_init(problem.getNumberOfVariables()),
+number_of_threads(n_threads){}
 
 ParallelBranchAndBound::~ParallelBranchAndBound(){}
 
 tbb::task * ParallelBranchAndBound::execute() {
 
-    unsigned long  shared_work = 0;
     globalPool.setSizeEmptying((unsigned long) (number_of_threads * 2)); /** If the global pool reach this size then the B&B starts sending part of their work to the global pool. **/
     
     BranchAndBound BB_container(0, problem, branch_init);
@@ -54,7 +54,7 @@ tbb::task * ParallelBranchAndBound::execute() {
         BB_container.increaseNumberOfPrunedNodes(bb_in->getNumberOfPrunedNodes());
         BB_container.increaseNumberOfReachedLeaves(bb_in->getNumberOfReachedLeaves());
         BB_container.increaseNumberOfUpdatesInLowerBound(bb_in->getNumberOfUpdatesInLowerBound());
-        shared_work += bb_in->getSharedWork();
+        BB_container.increaseSharedWork(bb_in->getSharedWork());
 	}
     
     BB_container.setParetoFrontFile(outputParetoFile);
@@ -65,7 +65,6 @@ tbb::task * ParallelBranchAndBound::execute() {
 	BB_container.saveParetoFront();
 	BB_container.saveSummarize();
     bb_threads.clear();
-    printf("Shared work: %lu\n", shared_work);
     printf("Data swarm recollected and saved.\n");
 	printf("Parallel Branch And Bound ended.\n");
 	return NULL;

ParallelBaB/ParallelBranchAndBound.hpp
@@ -27,7 +27,7 @@ public:
     ProblemFJSSP problem;
     Interval branch_init;
     
-    ParallelBranchAndBound(const ProblemFJSSP& problem);
+    ParallelBranchAndBound(int n_threads, const ProblemFJSSP& problem);
     ~ParallelBranchAndBound();
     tbb::task* execute();
     

ParallelBaB/main.cpp
@@ -93,8 +93,7 @@ int main(int argc, const char * argv[]) {
         //tbb::task_scheduler_init::default_num_threads();
 		tbb::task_scheduler_init init(number_of_threads);
 
-		ParallelBranchAndBound * pbb = new (tbb::task::allocate_root()) ParallelBranchAndBound(problem);
-		pbb->setNumberOfThreads(number_of_threads);
+		ParallelBranchAndBound * pbb = new (tbb::task::allocate_root()) ParallelBranchAndBound(number_of_threads, problem);
 		pbb->setParetoFrontFile(outputFile.c_str());
 		pbb->setSummarizeFile(summarizeFile.c_str());
 